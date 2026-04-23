eccb3e0c7610587c670006d0dd30e1597c783489
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2026-04-23 21:23:05 +0300

    feat: add explicit AI crawler allow rules to robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,15 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: '/api/' },
+    rules: [
+      { userAgent: '*', allow: '/', disallow: '/api/' },
+      { userAgent: 'GPTBot', allow: '/' },
+      { userAgent: 'ClaudeBot', allow: '/' },
+      { userAgent: 'PerplexityBot', allow: '/' },
+      { userAgent: 'CCBot', allow: '/' },
+      { userAgent: 'Google-Extended', allow: '/' },
+      { userAgent: 'Amazonbot', allow: '/' },
+    ],
     sitemap: 'https://calckit.yaro-labs.com/sitemap.xml',
   }
 }
